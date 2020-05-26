60c3d716929307ad7059b50eedfa91b927de6bd4
Shiro Han <shan99business@gmail.com>
2020-05-26 23:44:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,12 +22,17 @@ end
 
 def get_japanese_emoticon(path, english_emote)
   # code goes here
+  output = ""
   myHash = load_library(path)
   myHash.each do |name, data|
     data.each do |language, emoticon|
+      if english_emote == emoticon
+        data[:japanese]
+      end
       binding.pry
     end
   end
+  output
 end
 
 def get_english_meaning
